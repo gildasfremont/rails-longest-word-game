c52dbdbaaa55a4409aab2b0bfc41c506e7b52444
gildas fremont <gildas.fremont@gmail.com>
2016-08-08 16:34:47 +0200

    stored a value server side

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -6,21 +6,22 @@ class GameController < ApplicationController
     grid_size = 8
     @grid = []
     (1..grid_size).each { @grid << ('a'..'z').to_a.sample }
+    session[:grid] = @grid
   end
 
   def score
-    @time = Time.now.to_i - params[:start_time].to_i
-    answer = params[:answer]
-    letters = params[:letters]
+    @time = ((Time.now.to_i - params[:start_time].to_i) / 600_000_000).to_f
+    @answer = params[:answer]
+    letters = session[:grid]
     @score = 0
     @translation = "No translation"
-    wr = JSON.parse(open("http://api.wordreference.com/0.8/80143/json/enfr/#{answer.downcase}").read)
+    wr = JSON.parse(open("http://api.wordreference.com/0.8/80143/json/enfr/#{@answer.downcase}").read)
     if wr["term0"].nil?
       @message = "Not an english word"
-    elsif answer.downcase.split("").all? { |letter| answer.downcase.scan(letter.to_s).count <= letters.downcase.split("").join.scan(letter.to_s).count }
+    elsif @answer.downcase.split("").all? { |letter| @answer.downcase.scan(letter.to_s).count <= letters.join.downcase.split("").join.scan(letter.to_s).count }
       @message = "Well done"
       @translation = wr["term0"]["PrincipalTranslations"]["0"]["FirstTranslation"]["term"]
-      @score = answer.downcase.length * 100 - @time
+      @score = (@answer.downcase.length * 100 - @time).to_i
     else
       @message = "Not in the grid"
     end
